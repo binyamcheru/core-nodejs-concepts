d95e268b15f0df0553dc0f462ac36a5530bdded5
Binyam Cheru <binyamcheru123@gmail.com>
2026-02-12 17:22:30 +0300

    undersanding buffer concept with examples demonstrating allocation and usage

---
+++ buffer/concepts/different-allocs.js
@@ -0,0 +1,20 @@
+const { Buffer } = require("buffer");
+
+const buffer = Buffer.alloc(10000);
+
+console.log(Buffer.poolSize >>> 1);
+const unsafeBuffer = Buffer.allocUnsafe(10000);
+
+const buff = Buffer.allocUnsafeSlow(2);
+
+// other methods to create buffers:
+// Buffer.from();
+// Buffer.concat();
+
+for (let i = 0; i < unsafeBuffer.length; i++) {
+  if (unsafeBuffer[i] !== 0) {
+    console.log(
+      `Element at position ${i} has value: ${unsafeBuffer[i].toString(2)}`
+    );
+  }
+}

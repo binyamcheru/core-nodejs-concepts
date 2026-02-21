8194fd8c9be5d660a9fefc9e748bd1af6e3d0153
Binyam Cheru <binyamcheru123@gmail.com>
2026-02-21 20:58:05 +0300

    working with streams dealing with memory issues

---
+++ streams/concepts/readable-stream/readBig.js
@@ -0,0 +1,22 @@
+const fs = require("node:fs/promises");
+
+(async () => {
+  console.time("readBig");
+  const fileHandleRead = await fs.open("src.txt", "r");
+  const fileHandleWrite = await fs.open("dest.txt", "w");
+
+  const streamRead = fileHandleRead.createReadStream({
+    highWaterMark: 64 * 1024,
+  });
+  const streamWrite = fileHandleWrite.createWriteStream();
+
+  streamRead.on("data", (chunk) => {
+    if (!streamWrite.write(chunk)) {
+      streamRead.pause();
+    }
+  });
+
+  streamWrite.on("drain", () => {
+    streamRead.resume();
+  });
+})();
